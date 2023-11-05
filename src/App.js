db2f8ed07aad586250d03f8f98043fcf5cf2008b
Suraj Jha <sarcasticsuraj7048@gmail.com>
2023-11-05 14:31:27 +0530

    added files for document editor

--- src/App.js
+++ src/App.js
@@ -1,25 +1,21 @@
-import logo from './logo.svg';
-import './App.css';
+import TextEditor from './TextEditor';
+import {
+  BrowserRouter as Router,
+  Routes,//instead of switch in react-router-dom 6 and above
+  Route,
+  Navigate, //instead of Redirect in v6
+} from "react-router-dom"
+import { v4 as uuidV4 } from "uuid"
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+    <Router>
+      <Routes>
+        <Route path="/" element={<Navigate to={`/documents/${uuidV4()}`} />} />
+        <Route path="/documents/:id" element={<TextEditor />} />
+      </Routes>
+    </Router>
+  )
 }
 
 export default App;
